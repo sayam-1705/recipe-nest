8b505439536aecad0e011a23272be4c2677e00a6
sayam-1705 <sayam2022mondal@gmail.com>
2025-05-25 11:46:08 +0530

    Created user delete api

---
+++ src/app/api/user/delete/[email]/route.ts
@@ -0,0 +1,50 @@
+import { validateEmail, validatePassword } from "@/app/api/validations";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const reqSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ email: string }> }
+) {
+  const { email } = await params;
+  const { password } = await req.json();
+
+  const data = reqSchema.safeParse({ email, password });
+  if (!data.success) {
+    return NextResponse.json({ error: data.error }, { status: 400 });
+  }
+
+  if (!validateEmail(email)) {
+    return NextResponse.json(
+      { error: "Invalid email address" },
+      { status: 400 }
+    );
+  }
+  if (!validatePassword(password)) {
+    return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+  }
+
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    return NextResponse.json({ error: "Invalid Credentials" }, { status: 401 });
+  }
+
+  await User.deleteOne({ email: email });
+
+  return NextResponse.json(
+    { message: "User deleted successfully" },
+    { status: 200 }
+  );
+}
