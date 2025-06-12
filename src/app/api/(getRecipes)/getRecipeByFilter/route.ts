5423c052d5bf67fdcfd64d3320deb82a5bf62fdf
sayam-1705 <sayam2022mondal@gmail.com>
2025-06-12 21:31:08 +0530

    Created get recipes by filter, tested ok

---
+++ src/app/api/(getRecipes)/getRecipeByFilter/route.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from "next/server";
+import { dbConnect } from "../../mongodb";
+import { z } from "zod";
+import Recipe from "@/models/Recipe";
+
+const reqSchema = z.object({
+  name: z.string().optional(),
+  type: z.string().optional(),
+  meal: z.string().optional(),
+  time: z.string().optional(),
+  difficulty: z.string().optional(),
+  season: z.string().optional(),
+  occasion: z.string().optional(),
+  ingredients: z
+    .array(
+      z.object({
+        name: z.string().optional(),
+      })
+    )
+    .optional(),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    await dbConnect();
+
+    const body = await req.json();
+
+    const bodyData = reqSchema.safeParse(body);
+    if (!bodyData.success) {
+      return NextResponse.json({ error: bodyData.error }, { status: 400 });
+    }
+
+    const {
+      name,
+      type,
+      meal,
+      time,
+      difficulty,
+      season,
+      occasion,
+      ingredients,
+    } = bodyData.data;
+
+    const query: any = {};
+
+    if (name) query.name = { $regex: name, $options: "i" };
+    if (type) query.type = type;
+    if (meal) query.meal = meal;
+    if (time) query.time = time;
+    if (difficulty) query.difficulty = difficulty;
+    if (season) query.season = season;
+    if (occasion) query.occasion = occasion;
+    if (ingredients && ingredients.length > 0) {
+      query.ingredients = {
+        $elemMatch: {
+          name: { $in: ingredients.map((ingredient) => ingredient.name) },
+        },
+      };
+    }
+
+    const recipes = await Recipe.find(query);
+    const formattedRecipes = recipes.map((recipe) => ({ name: recipe.name }));
+
+    return NextResponse.json({ formattedRecipes }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
