8df510cdda3a776a9a64ed4795d2f7ea24ddf8c9
sayam-1705 <sayam2022mondal@gmail.com>
2025-07-13 19:46:18 +0530

    Created how it works page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Carousel from "@/components/carousel/Carousel";
+import HowItWorks from "@/components/howItWorks/HowItWorks";
 
 export default function Home() {
   return (
     <>
       <Carousel />
+      <HowItWorks />
     </>
   );
 }
